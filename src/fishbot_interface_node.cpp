cb48428385902400ef4e268468fd971145268a8e
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-05-06 15:49:05 +0200

    Update robot interface to send JSON strings to the robot

--- src/fishbot_interface_node.cpp
+++ src/fishbot_interface_node.cpp
@@ -9,17 +9,24 @@
 #include <bobi_msgs/PoseVec.h>
 #include <bobi_msgs/MotorVelocities.h>
 #include <bobi_msgs/EnableIR.h>
+#include <bobi_msgs/MaxAcceleration.h>
 #include <bobi_msgs/ProximitySensors.h>
 
 #include <iostream>
 #include <sstream>
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+namespace rpj = rapidjson;
 struct FishbotConfig {
     std::vector<double> led_colour = {0, 0, 255};
     std::string ip = "192.168.0.157";
     int port = 5623;
     int rate = 30;
     bool enable_ir = false;
+    double max_acceleration = 1.75;
 };
 
 FishbotConfig get_fishbot_config(const std::shared_ptr<ros::NodeHandle> nh)
@@ -30,6 +37,7 @@ FishbotConfig get_fishbot_config(const std::shared_ptr<ros::NodeHandle> nh)
     nh->param<int>("port", cfg.port, cfg.port);
     nh->param<int>("rate", cfg.rate, cfg.rate);
     nh->param<bool>("enable_ir", cfg.enable_ir, cfg.enable_ir);
+    nh->param<double>("max_acceleration", cfg.max_acceleration, cfg.max_acceleration);
     return cfg;
 }
 
@@ -46,6 +54,17 @@ public:
         _motor_vel_sub = _nh->subscribe("set_velocities", 5, &UDPCom::_motor_velocity_cb, this);
         _proximity_sensor_pub = nh->advertise<bobi_msgs::ProximitySensors>("proximity_sensors", 5);
         _enable_ir_srv = _nh->advertiseService("enable_ir", &UDPCom::_enable_ir_srv_cb, this);
+        _set_max_accel_srv = _nh->advertiseService("set_max_acceleration", &UDPCom::_set_max_accel_srv_cb, this);
+
+        // initialize json doc
+        _json_doc.SetObject();
+        rpj::Document::AllocatorType& allocator = _json_doc.GetAllocator();
+        rpj::Value object1(rpj::kObjectType);
+
+        _json_doc.AddMember("vl", 0., allocator);
+        _json_doc.AddMember("vr", 0., allocator);
+        _json_doc.AddMember("ir", _cfg.enable_ir, allocator);
+        _json_doc.AddMember("a", _cfg.max_acceleration, allocator);
     }
 
     void send(std::string command)
@@ -79,11 +98,19 @@ protected:
 
     void _motor_velocity_cb(const bobi_msgs::MotorVelocities::ConstPtr& motor_velocities)
     {
-        float left_motor_cm_per_s = motor_velocities->left * 100;
-        float right_motor_cm_per_s = motor_velocities->right * 100;
-        std::string cmd = _to_string_with_precision(left_motor_cm_per_s) + ":" + _to_string_with_precision(right_motor_cm_per_s) + ":" + std::to_string(_enable_ir);
+        float left_motor_cm_per_s = motor_velocities->left * 100.;
+        float right_motor_cm_per_s = motor_velocities->right * 100.;
 
-        send(cmd);
+        _json_doc["vl"].SetFloat(left_motor_cm_per_s);
+        _json_doc["vr"].SetFloat(right_motor_cm_per_s);
+        _json_doc["ir"].SetBool(_enable_ir);
+        _json_doc["a"].SetFloat(_cfg.max_acceleration);
+
+        rpj::StringBuffer cmd_json;
+        rpj::Writer<rpj::StringBuffer> writer(cmd_json);
+        _json_doc.Accept(writer);
+
+        send(cmd_json.GetString());
         if (_enable_ir) {
             _receive();
         }
@@ -94,16 +121,16 @@ protected:
         bobi_msgs::EnableIR::Response& res)
     {
         _enable_ir = req.enable;
+        _cfg.enable_ir = _enable_ir;
         return true;
     }
 
-    template <typename T>
-    std::string _to_string_with_precision(const T& val, const int n = 1)
+    bool _set_max_accel_srv_cb(
+        bobi_msgs::MaxAcceleration::Request& req,
+        bobi_msgs::MaxAcceleration::Response& res)
     {
-        std::ostringstream out;
-        out.precision(n);
-        out << std::fixed << val;
-        return out.str();
+        _cfg.max_acceleration = req.max_acceleration;
+        return true;
     }
 
     std::shared_ptr<ros::NodeHandle> _nh;
@@ -116,6 +143,9 @@ protected:
     ros::Subscriber _motor_vel_sub;
     ros::Publisher _proximity_sensor_pub;
     ros::ServiceServer _enable_ir_srv;
+    ros::ServiceServer _set_max_accel_srv;
+
+    rpj::Document _json_doc;
 };
 
 int main(int argc, char** argv)
